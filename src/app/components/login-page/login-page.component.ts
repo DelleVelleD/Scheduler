8038bc9d8e2b41e20192b786b394639dcc1f2a94
Taisann <thebackup115@gmail.com>
2020-09-13 22:09:07 -0400

    Created a template for a dropdown menu that allows the user to select between spring2019 and fall2019, but it's currently in the login page and needs to be put in the Faculty and Admin pages once they're done.

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  //Get rid of the "private router : Router" once the other pages are ready for modification
+  //constructor() {}
+  constructor(private router : Router) { }
 
   ngOnInit() {
 
   }
+  //Put these routers/navigate into their respective Admin and Faculty Calendar pages when ready
+  navigate(){
+    this.router.navigate(["courses-offered"])
+  }
+
+  navigate2(){
+    this.router.navigate(["courses-upcoming"])
+  }
+
 
 }
