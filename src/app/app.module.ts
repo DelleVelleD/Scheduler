428d635b70bc3c863356dcf5c33674897e161452
dellevelled <delle.caj@gmail.com>
2020-10-13 23:53:08 -0400

    Fix merge errors; Remove old homepage buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,21 +13,15 @@ import { Routes, RouterModule } from '@angular/router/';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { ScheduleConflictsComponent } from './components/schedule-conflicts/schedule-conflicts.component';
-//import { FacultyIssuesComponent } from './components/faculty-issues/faculty-issues.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
-import { LoginPageInvalidComponent } from './components/login-page/login-page-invalid.component';
 
-//import { CalendarComponent } from './components/calendar/calendar.component';
 import { CoursesOfferedComponent } from './components/courses-offered/courses-offered.component';
 import { CoursesUpcomingComponent } from './components/courses-upcoming/courses-upcoming.component';
 import { CurrentScheduleComponent } from './components/current-schedule/current-schedule.component';
-import { CurrentScheduleSplitComponent } from './components/current-schedule/current-schedule-split.component';
-//import { HomepageFacultyComponent } from './components/homepage-faculty/homepage-faculty.component';
 import { HomepageAdminComponent } from './components/admin/homepage-admin.component';
 import { FacultyComponent } from './components/faculty/faculty.component';
 
 import { CourselistService } from './components/full-courselist/courselist.service';
-
 import { CoursesPipe } from './components/full-courselist/courses/courses.pipe';
 import { CourseSectionsPipe } from './components/full-courselist/course-sections/course-sections.pipe';
 
@@ -53,35 +47,24 @@ const routes: Routes = [
   {
     path: 'main-page', component: MainPageComponent,
     children: [
-      
-      //{ path: 'faculty-issues', component: FacultyIssuesComponent }, ARCHIVED
-      
-      
       { path: 'full-courselist/course-sections/:CRSENO', component: CourseSectionsComponent },
       { path: 'full-courselist/course-sections/:DAYS', component: CourseSectionsComponent },
-      //{ path: "calendar", component: CalendarComponent }, ARCHIVED
-      { path: "upload-sheet", component: UploadSheetComponent }
     ]
   },
   { path: 'schedule-conflicts', component: ScheduleConflictsComponent },
   { path: "upload-sheet", component: UploadSheetComponent },
   { path: 'full-courselist/course-sections', component: CourseSectionsComponent },
   { path: 'full-courselist/courses', component: CoursesComponent },
-  //{ path: 'homepage-faculty', component: HomepageFacultyComponent }, ARCHIVED
   { path: 'admin', component: HomepageAdminComponent },
   { path: 'faculty', component: FacultyComponent },
   { path: 'login-page', component: LoginPageComponent },
-  { path: 'login-page-invalid', component: LoginPageInvalidComponent },
   { path: "current-schedule", component: CurrentScheduleComponent },
-  { path: "current-schedule-split", component: CurrentScheduleSplitComponent },
   { path: "courses-upcoming", component: CoursesUpcomingComponent },
   { path: "courses-offered", component: CoursesOfferedComponent },
-  //{ path: "calendar", component: CalendarComponent }, ARCHIVED
   { path: "view-hours", component: ViewHoursComponent },
   { path: "available-hours", component: AvailableHoursComponent },
   { path: "courses-upcoming-admin", component: CoursesUpcomingAdminComponent },
   { path: "courses-offered-admin", component: CoursesOfferedAdminComponent },
-  //{ path: '#', redirectTo: 'homepage-faculty', pathMatch: 'full' },
   { path: '', redirectTo: 'login-page', pathMatch: 'full' }
 ];
 
@@ -95,24 +78,18 @@ const routes: Routes = [
     HttpClientModule,
     RouterModule,
     RouterModule.forRoot(routes)
-    //  AppRoutingModule,
 
   ],
   declarations: [
     AppComponent,
     MainPageComponent,
     ScheduleConflictsComponent,
-    //FacultyIssuesComponent,
     CoursesComponent,
     CourseSectionsComponent,
     LoginPageComponent,
-    LoginPageInvalidComponent,
-    //CalendarComponent,
     CoursesOfferedComponent,
     CoursesUpcomingComponent,
     CurrentScheduleComponent,
-    CurrentScheduleSplitComponent,
-    //HomepageFacultyComponent,
     HomepageAdminComponent,
     FacultyComponent,
     CoursesPipe,
@@ -134,7 +111,5 @@ const routes: Routes = [
 export class AppModule {
 
   constructor(router: Router) {}
-
-
 
 }
