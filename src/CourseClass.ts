18efbfa2d1b1950327d01272ccb2378e15f94cf8
Taylor Williams <twilli21@ggc.edu>
2020-11-05 04:23:18 -0500

    Added CourseClass.ts and changed the courses-offered page to get data from courses.json

---
+++ src/CourseClass.ts
@@ -0,0 +1,22 @@
+export class Courses
+{
+    semester: string;
+    courses: any =
+    {
+        crn: null,
+        subject: null,
+        courseNum: null,
+        title: null,
+        sectionNum: null,
+        days: null,
+        start: null,
+        end: null,
+        room: null,
+        credits: null,
+        maxStudents: null,
+        professors:
+        {
+            info: null
+        }
+    };
+}
