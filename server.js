371ab131afb2fa912410e7c2187e9139db11402b
dellevelled <delle.caj@gmail.com>
2020-11-10 12:17:37 -0500

    Update to deploy on heroku

--- server.js
+++ server.js
@@ -7,16 +7,18 @@ var mongoose = require('mongoose');
 //var course = require('./routes/api/course');
 
 var app = express();
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 var db = require('./config/keys').mongoURI;
 
  //connection
+ /*
  mongoose.connect(db, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
 //connection message
+
 mongoose.connection.once('open', function(){
 	console.log('Successfully connected to database.');
 	db.collection('spring2019').find({}).toArray(function (err, result) 
@@ -34,19 +36,22 @@ var CourseModel = mongoose.model('Course');
 
 CourseModel.find({}).all;
 CourseModel.find(function (err, result) {
-  console.log('Hola',result);
+  console.log('Error in finding course model',result);
 }).all;
+*/
 
 /*
-Express server( name of my app=sd1AngularSillyDemo in dist folder )
+Express server( name of my app=ggc-scheduler in dist folder )
 
 */
-app.use(express.static(__dirname + '/dist/sd1AngularSillyDemo'));
+app.use(express.static(__dirname + '/dist/ggc-scheduler'));
 
-app.get('/*', (req, res) =>
-res.sendFile(path.join(__dirname)));
+app.get('/*', function(req, res) {
+	res.sendFile(path.join(__dirname+'/dist/ggc-scheduler/index.html'));
+});
 
-
+app.listen(port, ()=> console.log('Running'));
+/*
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -54,3 +59,4 @@ var server= http.createServer(app);
 
 server.listen(port, () =>  console.log('Running'));
 
+*/
